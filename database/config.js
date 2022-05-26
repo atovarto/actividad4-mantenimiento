48b6d9dc7fac3ff0190cf479e4533cc362b5fd32
Astrid Tovar <atovarto@ibero.edu.co>
2022-05-25 22:23:49 -0500

    Se inicia el proyecto, se configura express y la base de datos

---
+++ database/config.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const dbConnection = async () => {
+  try {
+    await mongoose.connect(
+      process.env.DB_CNN,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+      }
+    );
+
+    console.log("BD conectada.");
+  } catch (error) {
+    console.log(error);
+    throw new Error("Error al conectarse a la BD.");
+  }
+};
+
+module.exports = {
+  dbConnection,
+};
